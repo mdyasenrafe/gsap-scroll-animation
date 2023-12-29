e5e1242a7d394f2148acbc0853b6c388142b5fa2
Md yasen rafe <mdyasenrafe@gmail.com>
2023-12-29 20:56:56 +0600

    staying same thing working

--- script.js
+++ script.js
@@ -1,52 +1,47 @@
 gsap.registerPlugin(ScrollTrigger);
 
-const container = document.querySelector(".container");
+// Select the text elements
 const textThis = document.getElementById("textThis");
 const textIs = document.getElementById("textIs");
 const textEpic = document.getElementById("textEpic");
 const textGsap = document.getElementById("textGsap");
+const container = document.querySelector(".container");
+
+// Initially hide all texts except the first one
+gsap.set([textIs, textEpic, textGsap], { autoAlpha: 0 });
 
 // Create a timeline for the text animations with synchronized background color changes
 const tl = gsap.timeline({
   scrollTrigger: {
-    trigger: ".container",
-    scrub: true,
+    trigger: container,
     start: "top top",
+    end: "+=4000", // Adjust as necessary for your content length
+    scrub: 1,
     pin: true,
-    end: () => `+=${container.offsetWidth}`,
   },
 });
 
-// Animate 'This' with blue background
-tl.to(
+// Timeline for 'This' and blue background
+tl.to(container, { backgroundColor: "blue", ease: "none" }).to(
   textThis,
-  { opacity: 1, duration: 1, backgroundColor: "blue", ease: "none" },
-  0
-)
-  .to(textThis, { opacity: 0, duration: 1, ease: "none" }, 1)
-  // Animate 'IS' with orange background
-  .to(
-    textIs,
-    { opacity: 1, duration: 1, backgroundColor: "orange", ease: "none" },
-    1
-  )
-  .to(textIs, { opacity: 0, duration: 1, ease: "none" }, 2)
-  // Animate 'EPIC' with green background
-  .to(
-    textEpic,
-    { opacity: 1, duration: 1, backgroundColor: "green", ease: "none" },
-    2
-  )
-  .to(textEpic, { opacity: 0, duration: 1, ease: "none" }, 3)
-  // Animate 'GSAP' with black background
-  .to(
-    textGsap,
-    { opacity: 1, duration: 1, backgroundColor: "black", ease: "none" },
-    3
-  );
+  { autoAlpha: 0, duration: 1 },
+  0.5
+);
 
-// Ensure the background color changes are applied to the container
-tl.to(container, { backgroundColor: "blue" }, 0)
-  .to(container, { backgroundColor: "orange" }, 1)
-  .to(container, { backgroundColor: "green" }, 2)
-  .to(container, { backgroundColor: "black" }, 3);
+// Timeline for 'IS' and orange background
+tl.to(container, { backgroundColor: "orange", ease: "none" }, 1)
+  .fromTo(textIs, { autoAlpha: 0 }, { autoAlpha: 1, duration: 1 }, 1)
+  .to(textIs, { autoAlpha: 0, duration: 1 }, 2);
+
+// Timeline for 'EPIC' and green background
+tl.to(container, { backgroundColor: "green", ease: "none" }, 2)
+  .fromTo(textEpic, { autoAlpha: 0 }, { autoAlpha: 1, duration: 1 }, 2)
+  .to(textEpic, { autoAlpha: 0, duration: 1 }, 3);
+
+// Timeline for 'GSAP' and black background
+tl.to(container, { backgroundColor: "red", ease: "none" }, 3).fromTo(
+  textGsap,
+  { autoAlpha: 0 },
+  { autoAlpha: 1, duration: 1 },
+  3
+);
