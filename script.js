a723c6664c5f220ad29f8e594f3592a31238b0a4
Md yasen rafe <mdyasenrafe@gmail.com>
2023-12-29 20:39:22 +0600

    scroll effect updated

--- script.js
+++ script.js
@@ -1,63 +1,52 @@
-// Ensure ScrollTrigger is registered
 gsap.registerPlugin(ScrollTrigger);
 
-// Select the elements
-const section = document.querySelector("section");
-const textThis = document.querySelector("#textToAnimate"); // Ensure you use the ID to select the text
-const textIs = document.createElement("h1"); // Create a new h1 element for 'IS'
-textIs.textContent = "IS";
-textIs.className = "text-6xl font-bold opacity-0"; // Initially invisible
-section.appendChild(textIs); // Append it to the section
+const container = document.querySelector(".container");
+const textThis = document.getElementById("textThis");
+const textIs = document.getElementById("textIs");
+const textEpic = document.getElementById("textEpic");
+const textGsap = document.getElementById("textGsap");
 
-// Background color animation
-gsap.fromTo(
-  section,
-  {
-    backgroundColor: "#0000FF", // start color
+// Create a timeline for the text animations with synchronized background color changes
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".container",
+    scrub: true,
+    start: "top top",
+    pin: true,
+    end: () => `+=${container.offsetWidth}`,
   },
-  {
-    backgroundColor: "#FFA500", // end color
-    scrollTrigger: {
-      trigger: section,
-      start: "top top",
-      end: "bottom top",
-      scrub: true,
-    },
-  }
-);
+});
 
-// Fade out 'THIS'
-gsap.fromTo(
+// Animate 'This' with blue background
+tl.to(
   textThis,
-  {
-    opacity: 1,
-  },
-  {
-    opacity: 0,
-    ease: "power2.inOut",
-    scrollTrigger: {
-      trigger: section,
-      start: "top center",
-      end: "bottom top",
-      scrub: true,
-    },
-  }
-);
+  { opacity: 1, duration: 1, backgroundColor: "blue", ease: "none" },
+  0
+)
+  .to(textThis, { opacity: 0, duration: 1, ease: "none" }, 1)
+  // Animate 'IS' with orange background
+  .to(
+    textIs,
+    { opacity: 1, duration: 1, backgroundColor: "orange", ease: "none" },
+    1
+  )
+  .to(textIs, { opacity: 0, duration: 1, ease: "none" }, 2)
+  // Animate 'EPIC' with green background
+  .to(
+    textEpic,
+    { opacity: 1, duration: 1, backgroundColor: "green", ease: "none" },
+    2
+  )
+  .to(textEpic, { opacity: 0, duration: 1, ease: "none" }, 3)
+  // Animate 'GSAP' with black background
+  .to(
+    textGsap,
+    { opacity: 1, duration: 1, backgroundColor: "black", ease: "none" },
+    3
+  );
 
-// Fade in 'IS'
-gsap.fromTo(
-  textIs,
-  {
-    opacity: 0,
-  },
-  {
-    opacity: 1,
-    ease: "power2.inOut",
-    scrollTrigger: {
-      trigger: section,
-      start: "top center",
-      end: "bottom top",
-      scrub: true,
-    },
-  }
-);
+// Ensure the background color changes are applied to the container
+tl.to(container, { backgroundColor: "blue" }, 0)
+  .to(container, { backgroundColor: "orange" }, 1)
+  .to(container, { backgroundColor: "green" }, 2)
+  .to(container, { backgroundColor: "black" }, 3);
