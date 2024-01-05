921bb4c23f380643dc34e01d56d399c2993c5138
Md yasen rafe <mdyasenrafe@gmail.com>
2024-01-05 11:47:43 +0600

    walking men wokring

--- script.js
+++ script.js
@@ -1,46 +1,34 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// Select the text elements
+// define const
 const textThis = document.getElementById("textThis");
 const textIs = document.getElementById("textIs");
 const textEpic = document.getElementById("textEpic");
 const textGsap = document.getElementById("textGsap");
-const container = document.querySelector(".container");
+const container = document.querySelector(".section");
 
 // Initially hide all texts except the first one
 gsap.set([textIs, textEpic, textGsap], { autoAlpha: 0 });
 
-// Create a timeline for the text animations with synchronized background color changes
+// create timeline
 const tl = gsap.timeline({
   scrollTrigger: {
     trigger: container,
     start: "top top",
-    end: "+=4000", // Adjust as necessary for your content length
+    end: "+=1000",
     scrub: 1,
     pin: true,
   },
 });
 
-tl.to(container, { backgroundColor: "blue", ease: "none" }).to(
-  textThis,
-  { autoAlpha: 0, duration: 1 },
-  0.5
-);
-
-// Timeline for 'IS' and orange background
-tl.to(container, { backgroundColor: "orange", ease: "none" }, 1)
+// define animation
+tl.to(container, { backgroundColor: "#0000FF", ease: "none" })
+  .to(textThis, { autoAlpha: 0, duration: 1 }, 0.5)
+  .to(container, { backgroundColor: "#FFA500", ease: "none" }, 1)
   .fromTo(textIs, { autoAlpha: 0 }, { autoAlpha: 1, duration: 1 }, 1)
-  .to(textIs, { autoAlpha: 0, duration: 1 }, 2);
-
-// Timeline for 'EPIC' and green background
-tl.to(container, { backgroundColor: "green", ease: "none" }, 2)
-  .fromTo(textEpic, { autoAlpha: 0 }, { autoAlpha: 1, duration: 1 }, 2)
-  .to(textEpic, { autoAlpha: 0, duration: 1 }, 3);
-
-// Timeline for 'GSAP' and black background
-tl.to(container, { backgroundColor: "red", ease: "none" }, 3).fromTo(
-  textGsap,
-  { autoAlpha: 0 },
-  { autoAlpha: 1, duration: 1 },
-  3
-);
+  .to(textIs, { autoAlpha: 0, duration: 1 }, 2)
+  .to(container, { backgroundColor: "#008000", ease: "none" }, 2.5)
+  .fromTo(textEpic, { autoAlpha: 0 }, { autoAlpha: 1, duration: 1 }, 2.5)
+  .to(textEpic, { autoAlpha: 0, duration: 1 }, 3.5)
+  .to(container, { backgroundColor: "#FF0000", ease: "none" }, 4)
+  .fromTo(textGsap, { autoAlpha: 0 }, { autoAlpha: 1, duration: 1 }, 4);
